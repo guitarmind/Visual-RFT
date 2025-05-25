a972aa1fe8818c8470bbf8653ffd8b2db6523350
Mark Peng <markpeng@exosite.com>
2025-05-25 12:40:50 +0800

    Fixed version issues. Worked on pytorch/pytorch:2.5.1-cuda12.4-cudnn9-devel image.

--- setup.sh
+++ setup.sh
@@ -11,5 +11,8 @@ pip install flash-attn --no-build-isolation
 pip install vllm==0.7.2
 
 # fix transformers version
-pip install transformers==4.46.0 datasets
-# pip install git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
+pip install git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
+
+# fixed by markpeng
+pip install trl==0.16.0
+pip install tensorboard
