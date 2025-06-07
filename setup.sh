82f9183b4a021155b68ab21f2375cedd9f8f8595
Mark Peng <markpeng@exosite.com>
2025-06-08 00:20:13 +0800

    Fixed Qwen2.5 VL issue.

--- setup.sh
+++ setup.sh
@@ -14,7 +14,8 @@ pip install vllm==0.7.2
 # pip install git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
 # fix Qwen2.5 VL issue
 # https://github.com/huggingface/transformers/issues/36257
-pip install git+https://github.com/huggingface/transformers.git@8ee50537fe7613b87881cd043a85971c85e99519
+# pip install git+https://github.com/huggingface/transformers.git@8ee50537fe7613b87881cd043a85971c85e99519
+pip install transformers==4.51.3
 
 # fixed by markpeng
 pip install trl==0.16.0
